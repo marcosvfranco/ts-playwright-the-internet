4bc58b001218ab988f59eb7fa6dac00e76d25554
Marcos Franco <marcos_franco@apple.com>
2022-06-04 22:19:09 -0300

    task: keypress (input) tests

---
+++ tests/keypress.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('when opening key_presses page', () => {
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://the-internet.herokuapp.com/key_presses');
+  })
+
+  test.only('and an input is displayed, so something is typed and registered in the page', async ({ page }) => {
+
+    const input = page.locator('input#target');
+    const characterToBeTyped = 'A';
+    await input.type(characterToBeTyped);
+
+    expect(await page.locator('#result')).toHaveText(`You entered: ${characterToBeTyped}`);
+  });
+
+})
