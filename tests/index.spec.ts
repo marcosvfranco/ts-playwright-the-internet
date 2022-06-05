f1fd6ad7eaac5d9316e5c8c409d55f338229175e
Marcos Franco <marcos_franco@apple.com>
2022-06-04 22:18:58 -0300

    task: link and list tests in index page

---
+++ tests/index.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('when opening index page', () => {
+
+  test.beforeEach(async ({ page }, testInfo) => {
+    await page.goto('https://the-internet.herokuapp.com/');
+  })
+
+  test('then a page is loaded with a h1 title and h2 subtitle', async ({ page }) => {
+    const title = page.locator('h1.heading');
+    const subTitle = page.locator('h2 >> text=Available Examples');
+    await expect(title).toHaveText('Welcome to the-internet');
+    await expect(subTitle).toBeVisible();
+  });
+
+  test('then a list of 44 links is displayed', async({ page }) => {
+    const linkList = await page.locator('div#content > ul > li');
+    for(let i = 0; i < await linkList.count(); i++) {
+      expect(linkList.nth(i)).toHaveProperty('href')
+    }
+    await expect(linkList).toHaveCount(44);
+  })
+})
