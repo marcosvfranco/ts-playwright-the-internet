33603403daef9a6003c9565a141916de63b78ea2
Marcos Franco <marcos_franco@apple.com>
2022-06-04 22:17:42 -0300

    task: adding initial configuration

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # ts-playwright-the-internet
+
+E2E Test Automation framework using TypeScript, [Playwright](https://playwright.dev/docs/intro) to test the website [The Internet](https://the-internet.herokuapp.com/)
+
+You can use and apply it to test another real world websites!
+
+## Code Quality:
+
+[ESLint](https://eslint.org/docs/user-guide/configuring/)
+
+## CI/CD
+TBD
---
+++ playwright.config.ts
@@ -0,0 +1,51 @@
+// @ts-check
+import { PlaywrightTestConfig, devices } from '@playwright/test';
+import { Options } from './configs/customTest';
+
+
+/**
+ * Read environment variables from file.
+ * https://github.com/motdotla/dotenv
+ */
+// require('dotenv').config();
+
+
+/**
+ * @see https://playwright.dev/docs/test-configuration
+ * @type {import('@playwright/test').PlaywrightTestConfig}
+ */
+ const config: PlaywrightTestConfig<Options>  = {
+  use: {
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    trace: 'on',
+    launchOptions: {
+      slowMo: 50,
+    },
+  },
+  testDir: 'tests',
+
+  workers: 4,
+  /* Configure projects for major browsers */
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'],
+      },
+    },
+  ]
+
+  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
+  // outputDir: 'test-results/',
+
+  /* Run your local dev server before starting the tests */
+  // webServer: {
+  //   command: 'npm run start',
+  //   port: 3000,
+  // },
+};
+
+export default config;
